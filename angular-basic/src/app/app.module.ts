b8af9582c72de4d3537c05a9c781affe8ad56e3d
cheulong <searcheulong@gmail.com>
2018-11-16 01:38:40 +0700

    Assignment2

--- angular-basic/src/app/app.module.ts
+++ angular-basic/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +42,7 @@ import { PromotionService } from './services/promotion.service';
     MatToolbarModule,
     FlexLayoutModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
